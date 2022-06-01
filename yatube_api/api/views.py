8ad2871f5386491f0d0a1bba8b4a92a9ad2ccacf
Гут Владимир <vladimir.monolith@yandex.ru>
2022-06-01 20:46:56 +0300

    Третье ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, permissions, viewsets
 
 from posts.models import Group, Post
-
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
