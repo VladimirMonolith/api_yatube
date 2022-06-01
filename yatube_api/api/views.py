d373cb62fea01afabf9d3150caf4bc658d50e62d
Гут Владимир <vladimir.monolith@yandex.ru>
2022-06-01 16:17:32 +0300

    Второе ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, permissions, viewsets
 
+from posts.models import Group, Post
+
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 )
-
-from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
